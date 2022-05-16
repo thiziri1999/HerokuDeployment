8145720e9ae240aa794f449a3b85bd429986bb92
thiziri1999 <79649676+thiziri1999@users.noreply.github.com>
2022-05-15 23:12:02 -0400

    six

--- PortfoliosAPP/views.py
+++ PortfoliosAPP/views.py
@@ -3,13 +3,13 @@ from .models import Project
 # Create your views here.
 def home(request):
     projects =Project.objects.all()
-    return render (request,'Portfolio/home.html',{'projects':projects})
+    return render (request,'PortfoliosAPP/home.html',{'projects':projects})
 def detail(request,idProjet):
     project=get_object_or_404(Project,pk=idProjet)
-    return render (request,'Portfolio/detail.html',{'project':project})
+    return render (request,'PortfoliosAPP/detail.html',{'project':project})
 
 def test(request):
     projects =Project.objects.all()
-    return render (request,'Portfolio/test.html',{'projects':projects})
+    return render (request,'PortfoliosAPP/test.html',{'projects':projects})
 
   
