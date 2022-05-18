e8956d183fb20db2a2fb6bd7ae12a3ed5f202e58
thiziri1999 <79649676+thiziri1999@users.noreply.github.com>
2022-05-18 10:33:51 -0400

    Inff

--- PortFolio/urls.py
+++ PortFolio/urls.py
@@ -21,6 +21,7 @@ from PortfoliosAPP import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('/home.html',views.home,name='home'),
      path('test',views.test,name='test'),
    
 
